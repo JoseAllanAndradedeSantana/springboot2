5ededfbd82f2fb49a82ea6be0c822972f0df030a
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2021-11-05 20:46:30 -0300

    includo RestTempalte exchange.

--- src/main/java/allanjose/springboot2/client/SpringClient.java
+++ src/main/java/allanjose/springboot2/client/SpringClient.java
@@ -2,8 +2,13 @@ package allanjose.springboot2.client;
 
 import allanjose.springboot2.domain.Anime;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Log4j2
 public class SpringClient {
@@ -13,6 +18,21 @@ public class SpringClient {
 
         Anime ob = new RestTemplate().getForObject("http://localhost:8080/animes/2", Anime.class);
         log.info(ob);
+//
+//        ResponseEntity<List<Anime>> ex = new RestTemplate().exchange("http://localhost:8080/animes", HttpMethod.GET, null,
+//                new ParameterizedTypeReference<List<Anime>>() {
+//                });
+//        log.info(ex.getBody());
+//
+//
+//        Anime novoNome = Anime.builder().name("José Allan").build();
+//        Anime novoNomeSalvo = new RestTemplate().postForObject("http://localhost:8080/animes",
+//                novoNome, Anime.class);
+
+        Anime novoNome = Anime.builder().name("José Allan").build();
+        ResponseEntity<Anime> novoNomeSalvo = new RestTemplate().exchange("http://localhost:8080/animes",
+                HttpMethod.POST, new HttpEntity<>(novoNome), Anime.class);
+
 
     }
 }
